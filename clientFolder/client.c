7aab5f923609d70a24f809e3645de129f8db79e7
Rishi Soni <rishisoni1003@gmail.com>
2017-09-11 01:32:18 -0600

    Sending file from client to server

--- clientFolder/client.c
+++ clientFolder/client.c
@@ -11,18 +11,22 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <errno.h>
+#include <string.h>
 
-#define MAXBUFSIZE 100
+#define MAXBUFSIZE	(100)
 
-/* You will have to modify the program below */
 
 int main (int argc, char * argv[])
 {
-	int errsv;
+	char command[100];
+	char data[512];
+	int len[1];
+	int errsv;									//Store errno
 	int sbytes = -1;                             // number of bytes send by sendto()
 	int rbytes = -1;                             // number of bytes send by recvfrom()
 	int sockfd;                               //this will be our socket
 	char buffer[MAXBUFSIZE];
+	FILE *fp;
 
 	struct sockaddr_in remote;              //"Internet socket address structure"
 
@@ -37,7 +41,8 @@ int main (int argc, char * argv[])
 	  information regarding where we'd like to send our packet 
 	  i.e the Server.
 	 ******************/
-	bzero(&remote,sizeof(remote));               //zero the struct
+	// bzero(&remote,sizeof(remote));               //zero the struct
+	memset(&remote,0, sizeof(remote));
 	remote.sin_family = AF_INET;                 //address family
 	remote.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
 	remote.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
@@ -49,38 +54,43 @@ int main (int argc, char * argv[])
 		printf("unable to create client socket");
 	}
 
-	/******************
-	  sendto() sends immediately.  
-	  it will report an error if the message fails to leave the computer
-	  however, with UDP, there is no error if the message is lost in the network once it leaves the computer.
-	 ******************/
-	char command[] = "apple is the shit";	
-	sbytes = sendto(sockfd, command, (sizeof(command) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
-
-	if(sbytes < 0)
-		printf("unable to send over UDP\n");
-
-	// Blocks till bytes are received
 	struct sockaddr_in from_addr;
 	socklen_t addr_length = sizeof(struct sockaddr);
 	bzero(buffer,sizeof(buffer));
 
+
+	strcpy(command, "Sending a text file");
+	printf("%s\n", command);
+	sbytes = sendto(sockfd, command, (sizeof(command) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+
 	rbytes = recvfrom(sockfd, buffer, (int)MAXBUFSIZE, 0, (struct sockaddr *)&from_addr, &addr_length);  
+	printf("Server: %s\n", buffer);
 
-	if(rbytes < 0)
+	if(strcmp(buffer, "Okay") == 0)
 	{
-		errsv = errno;
-		printf("error in receiving data over UDP\n");
-		printf("The error number is %d\n", errsv);
+		// memset(command, 0, sizeof(command));
+		// sbytes = sendto(sockfd, len, (sizeof(command) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+		fp = fopen("foo1", "r");		//open file in read mode
+
+		if(fp == NULL)
+		{
+			perror("could not open file");
+			exit(1);
+		}
+
+	   	fseek(fp, 0, SEEK_END);
+   		len[0] = ftell(fp);
+		printf("Length of file is %d bytes\n", len[0]);
+		sbytes = sendto(sockfd, len, (sizeof(len) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+		fseek(fp, 0, SEEK_SET);
+		while(fgets(data, 512, fp) !=NULL)
+		{
+			sbytes = sendto(sockfd, data, (sizeof(data) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+			// printf("%s", data);
+			memset(data, 0, sizeof(data));
+		}
+
 	}
-	else if (rbytes == 0)
-		printf("no data received on the socket\n");
 
-	printf("client: number of bytes received: %d\n", rbytes);
-
-	printf("Server says %s\n", buffer);
-	
 	close(sockfd);
-
 }
-
--- serverFolder/server.c
+++ serverFolder/server.c
@@ -12,19 +12,19 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
-/* You will have to modify the program below */
 
-#define MAXBUFSIZE 100
+#define MAXBUFSIZE	(512)
 
 int main (int argc, char * argv[] )
 {
 
-
+	int file_size[1], len;
 	int sockfd;                           //This will be our socket
 	struct sockaddr_in sin, remote;     //"Internet socket address structure"
 	unsigned int remote_length;         //length of the sockaddr_in structure
 	int nbytes;                        //number of bytes we receive in our message
 	char buffer[MAXBUFSIZE];             //a buffer to store our received message
+	FILE *fp;
 	if (argc != 2)
 	{
 		printf ("USAGE:  <port>\n");
@@ -35,7 +35,7 @@ int main (int argc, char * argv[] )
 	  This code populates the sockaddr_in struct with
 	  the information about our socket
 	 ******************/
-	bzero(&sin,sizeof(sin));                    //zero the struct
+	memset(&sin,0, sizeof(sin));                    //zero the struct
 	sin.sin_family = AF_INET;                   //address family
 	sin.sin_port = htons(atoi(argv[1]));        //htons() sets the port # to network byte order
 	sin.sin_addr.s_addr = INADDR_ANY;           //supplies the IP address of the local machine
@@ -60,18 +60,40 @@ int main (int argc, char * argv[] )
 	remote_length = sizeof(remote);
 
 	//waits for an incoming message
-	bzero(buffer,sizeof(buffer));
+	// bzero(buffer,sizeof(buffer));
+	// nbytes = recvfrom(sockfd, buffer, (int)MAXBUFSIZE, 0, (struct sockaddr *)&remote, &remote_length);
+
+	// printf("The client says %s\n", buffer);
+	// printf("server: number of bytes sent: %d\n", nbytes);
+
+
+	char msg[100];
+	// nbytes = sendto(sockfd, msg, (sizeof(msg) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+
+	memset(buffer,0, sizeof(buffer));
 	nbytes = recvfrom(sockfd, buffer, (int)MAXBUFSIZE, 0, (struct sockaddr *)&remote, &remote_length);
+	printf("Client: %s\n", buffer);
 
-	printf("The client says %s\n", buffer);
-	printf("server: number of bytes sent: %d\n", nbytes);
-
-
-	char msg[] = "orange is the new black";
+	strcpy(msg, "Okay");
 	nbytes = sendto(sockfd, msg, (sizeof(msg) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
 
-	if (nbytes < 0)
-		printf("server error: message not sent\n");
+	memset(file_size, 0, sizeof(file_size));
+	nbytes = recvfrom(sockfd, file_size, sizeof(file_size), 0, (struct sockaddr *)&remote, &remote_length);
+	printf("The size of the file to be received is %d\n", file_size[0]);
+	len = file_size[0];
+	
+
+	memset(buffer, 0, sizeof(buffer));
+	printf("Writing received data in hello_server.txt\n");
+	fp = fopen("hello_server.txt", "w");
+	while(len > 0)
+	{
+		nbytes = recvfrom(sockfd, buffer, (int)MAXBUFSIZE, 0, (struct sockaddr *)&remote, &remote_length);
+		// printf("%s", buffer);
+		fputs(buffer, fp);
+		len = len - strlen(buffer);
+		memset(buffer, 0, sizeof(buffer));
+		printf("%d\n", len);	}
 
 	close(sockfd);
 }
