e6b86181bf82e3d977585760c51a08d40c554a78
Rishi Soni <rishisoni1003@gmail.com>
2017-10-21 20:09:07 -0600

    Added multi-client support

--- webserver.c
+++ webserver.c
@@ -1,9 +1,11 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
 #include <error.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>	//inet_addr
@@ -13,19 +15,107 @@
 char *ROOT;
 FILE *fp;
 
+struct sockaddr_in server, client;
+int sockfd, new_sockfd, c, *new_sockfd2, i;
+char *msg, PORT[6], client_reply[200];
+int clients[NUM_CLIENTS];
+long ret;
+char buffer[BUFSIZE+1], local_buffer[BUFSIZE+1], *substring, *file_name;
+char *data_buffer;
+int count = 0, file_len;
+size_t len;
+
+void signal_handler(int signum)
+{
+	if(signum == SIGINT)
+	{
+		printf("Cleaning up...\n");
+		if (fp != NULL)
+		{	
+			fclose(fp);
+			printf("File closed\n");
+		}
+		else
+			printf("File is already in closed state\n");
+
+	    printf("Exiting....\n");
+		close(new_sockfd);
+	    close(sockfd);
+	    exit(0);
+	}
+}
+
+
+void server_response(int new_sockfd)
+{
+	ret = recv(new_sockfd, buffer, BUFSIZE, 0);
+	if(ret == 0 || ret == -1)
+	{
+		perror("browser request read failed");
+		exit(1);
+	}
+
+	//Extracting just the HTTP request from client
+	for (i = 0; i < ret; i++)
+	{
+		if(buffer[i] == '\n')
+		{
+			local_buffer[i] = '\0';
+			break;
+		}
+		else 
+		{
+			local_buffer[i] = buffer[i];
+		}
+	}
+	
+	if(strstr(local_buffer, "GET"))
+	{
+		// printf("GET found\n");
+		substring = strcasestr(local_buffer, "index.html");
+		// if (substring)
+		// {
+		// 	printf("index.html requested\n");
+		// 	len = local_buffer - substring;
+		// 	string2 = malloc(len + 1);
+		// 	memcpy(string2, local_buffer, len);
+		// 	string2[len] = 0;
+		// }
+		file_name = malloc(10);
+		// printf("Copying file name\n");
+		memcpy(file_name, substring, 10);
+		printf("%s", file_name);
+		fp = fopen(file_name, "r");		//open file in read mode
+		
+		if(fp == NULL)
+		{
+			perror("File does not exist");
+			exit(1);
+		}
+
+		fseek(fp, 0, SEEK_END);
+		file_len = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+
+		// printf("%d\n", file_len);
+		data_buffer = malloc(file_len);
+		fread(data_buffer, 1, file_len, fp);
+
+		// printf("%s", data_buffer);
+
+		send(new_sockfd, "HTTP/1.1 200 OK\n", 16, 0);
+		send(new_sockfd, "Content-Type: text/html\n", 24, 0);
+		send(new_sockfd, "Content-Length: 3391\n\n", 22, 0);
+		send(new_sockfd, data_buffer, 3391, 0);
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	struct sockaddr_in server, client;
-	int sockfd, new_sockfd, c, *new_sockfd2, i;
-	char *msg, PORT[6], client_reply[200];
-	int clients[NUM_CLIENTS];
-	long ret;
-	char buffer[BUFSIZE+1], local_buffer[BUFSIZE+1], *substring, *file_name;
-	char *data_buffer;
-	int count = 0, file_len;
-	size_t len;
+	//Registering signal handler
+	signal(SIGINT, signal_handler);
 
-	//Parse from ws.conf file instead
+	/*** Parse from ws.conf file instead ***/
 	ROOT = getenv("PWD"); //record current working directory
 	strcpy(PORT, "80");
 
@@ -57,95 +147,25 @@ int main(int argc, char const *argv[])
 	c = sizeof(struct sockaddr_in);
 	memset(buffer, 0, sizeof(buffer));
 	memset(local_buffer, 0, sizeof(local_buffer));
+	new_sockfd = 1;
 	
-	while((new_sockfd = accept(sockfd, (struct sockaddr *)&client, (socklen_t*)&c)))
+	while(new_sockfd)
 	{
-		fflush(stdin);
-		// printf("Connection accepted\n");
-		ret = read(new_sockfd, buffer, BUFSIZE);
-		if(ret == 0 || ret == -1)
-		{
-			perror("browser request read failed");
-			exit(1);
-		}
-
-		//Extracting HTTP request from client
-		for (i = 0; i < ret; i++)
-		{
-			if(buffer[i] == '\n')
-			{
-				local_buffer[i] = '\0';
-				break;
-			}
-			else 
-			{
-				local_buffer[i] = buffer[i];
-			}
-		}
+		new_sockfd = accept(sockfd, (struct sockaddr *)&client, (socklen_t*)&c);
 		
-		// printf("%s", local_buffer);
-		// while(count != i)
-		// {
-		// 	printf("%c", local_buffer[count]);
-		// 	// printf("%s", local_buffer);
-		// 	// printf("Loop\n");
-		// 	count++;
-		// 	// i--;
-		// }
-
-		if(strstr(local_buffer, "GET"))
+		if(fork() == 0)
 		{
-			// printf("GET found\n");
-			substring = strstr(local_buffer, "index.html");
-			// if (substring)
-			// {
-			// 	printf("index.html requested\n");
-			// 	len = local_buffer - substring;
-			// 	string2 = malloc(len + 1);
-			// 	memcpy(string2, local_buffer, len);
-			// 	string2[len] = 0;
-			// }
-			file_name = malloc(10);
-			memcpy(file_name, substring, 10);
-			printf("%s", file_name);
-	     
-	        // file_name[strcspn(file_name, " ")] = 0;
-			fp = fopen(file_name, "r");		//open file in read mode
-			
-			if(fp == NULL)
-			{
-				perror("File does not exist");
-				exit(1);
-			}
-
-			fseek(fp, 0, SEEK_END);
-   			file_len = ftell(fp);
-			fseek(fp, 0, SEEK_SET);
-
-			printf("%d\n", file_len);
-			data_buffer = malloc(file_len);
-			fread(data_buffer, 1, file_len, fp);
-
-			printf("%s", data_buffer);
-
-			write(new_sockfd, "HTTP/1.1 200 OK\n", 16);
-			write(new_sockfd, "Content-Type: text/html\n", 24);
-			write(new_sockfd, "Content-Length: 3391\n\n", 22);
-			write(new_sockfd, data_buffer, 3391);
+			close(sockfd);		//child closes listening socket
+			server_response(new_sockfd);
+			close(new_sockfd);
+			exit(0);
 		}
-		break;
-		// write(new_sockfd, msg, strlen(msg));
-	}
 
-	if (new_sockfd < 0)
-	{
-		perror("accept failed");
-		exit(1);
-	}
+		close(new_sockfd);	
+		// printf("Response sent\n");
+		// memset(buffer, 0, sizeof(buffer));
+		// printf("Memory cleared\n");
 
-	// printf("Connection accepted\n");
-	close(sockfd);
-	close(new_sockfd);
-	// sleep(1);
+	}
 	return 0;
 }
