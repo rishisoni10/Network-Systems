6e2b74d2f04a4be04a679960c7c9abc1742f9db0
Rishi Soni <rishisoni1003@gmail.com>
2017-10-22 20:15:47 -0600

    Few modifications

--- webserver.c
+++ webserver.c
@@ -6,7 +6,8 @@
 #include <string.h>
 #include <error.h>
 #include <signal.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
@@ -24,16 +25,19 @@ int sockfd, new_sockfd, c, *new_sockfd2;
 int clients[NUM_CLIENTS];
 int process_index;
 
-// int count = 0, file_len;
-// size_t len;
-void timeout_handler(int signum)
+void alarm_handler(int sig)
 {
-     printf("Socket Timed out\n");
-     printf("closing the socket %d\n",process_index); 
-     shutdown (multi_clients[process_index], SHUT_RDWR);         
-     close(multi_clients[process_index]);
-     multi_clients[process_index]=-1;
-     exit(0);    
+	if(sig == SIGALRM)
+	{
+		// signal(signum, alarm_handler);
+	    printf("Socket Timed out\n");
+	    printf("closing the socket %d\n",process_index); 
+	    shutdown (multi_clients[process_index], SHUT_RDWR);         
+	    close(multi_clients[process_index]);
+	    multi_clients[process_index] = -1;
+	    exit(0);    
+	}
+
 }
 
 void signal_handler(int signum)
@@ -53,6 +57,7 @@ void signal_handler(int signum)
 	    exit(0);
 	}
 }
+
 int file_size(FILE *file_fp)
 {
 	fseek(file_fp, 0, SEEK_END);
@@ -63,18 +68,20 @@ int file_size(FILE *file_fp)
 
 void http_server_send(int sock_desc, char *server_msg, FILE *send_fp)
 {
-	void *data_buffer;
+	char data_buffer[BUFSIZE];
 	printf("Server msg:\n");
+	int data_bytes;
 	printf("%s\n", server_msg);
 	int len = strlen(server_msg);
 	if(send(sock_desc, server_msg, len, 0) == -1)
 		printf("Send failed\n");
 	
-	int file_len = file_size(send_fp);
-	data_buffer = malloc(file_len + 1);
+	// int file_len = file_size(send_fp);
+	// data_buffer = malloc(file_len + 1);
 
-	fread(data_buffer, 1, file_len, send_fp);
-	send(sock_desc, data_buffer, file_len, 0);
+	// fread(data_buffer, 1, file_len, send_fp);
+	while((data_bytes = fread(data_buffer, 1, (int)BUFSIZE, send_fp))>0)
+		send(sock_desc, data_buffer, (int)BUFSIZE, 0);
 }
 
 char* config_file(char *string)
@@ -85,15 +92,8 @@ char* config_file(char *string)
 	char *buffer;
 	char *param, *tk;
 	conf_fp = fopen(filename, "r");
-
-	// fseek(conf_fp, 0, SEEK_END);
-	// int len = ftell(conf_fp);
-	// fseek(conf_fp, 0, SEEK_SET);
 	int len = file_size(conf_fp);
-
 	buffer = malloc(len);
-
-	// printf("file size in old function is %d\n", len);
 	fread(buffer, 1, len, conf_fp);
 	if((param = strstr(buffer,string)) != NULL)
 	{
@@ -110,18 +110,13 @@ char* http_file_format(char *hint_string, char *search_string)
 	char *param, *tk;
 	char *parse;
 	FILE *conf_fp;
-	char buffer[4000] = {0};
+	char buffer[100000] = {0};
 
 	conf_fp = fopen(filename, "r");
 	if (conf_fp == NULL)
 		printf("Error. Could not open file\n");
 
-	// fseek(conf_fp, 0, SEEK_END);
-	// int len = ftell(conf_fp);
-	// fseek(conf_fp, 0, SEEK_SET);
 	int len = file_size(conf_fp);
-
-	// buffer = malloc(len);
 	fread(buffer, 1, len, conf_fp);
 
 	if((param = strstr(buffer, hint_string)) != NULL)
@@ -131,31 +126,24 @@ char* http_file_format(char *hint_string, char *search_string)
 		tk = strtok(NULL, " \t\n");
 	}
 	fclose(conf_fp);
-	// printf("http file format is %s\n", tk);
 	return tk;
 }
 
 void server_response(int n, char* ROOT)
 {
-	char buffer[99999], *reqline[3], packet[BUFSIZE], path[99999], hdr[1000], msg[99999], post_msg[10000];
-	char size[10];
+	char size[10], buffer[99999], *reqline[3], path[10000], hdr[10000], msg[99999], post_msg[99999]; 	//String parsing using an array of three char pointers (*reqline[3])
 	int ret, len;
 	FILE *res_fp;
-	char *def_page = malloc(20);
-	char *file_name = malloc(100);
-	char *file_format = malloc(10);
-	char *token = malloc(20);
-	char *file_type = malloc(25);
+	char def_page[30], file_name[1000], error_msg[1000], always_connect[1000000];
+	char *token = malloc(30);
+	char *file_type = malloc(40);
 	char *content_type = malloc(50);
-	char *error_msg = malloc(5000);
 	char *http;
-	char *always_connect = malloc(99999);
 	char delimiter[] = ".";
-	char *time_out = NULL;
+	char *time_out_str = NULL;
 
-	memset(&buffer, 0, 99999);
-
-	ret = recv(multi_clients[n], buffer, 99999, 0);
+	memset(&buffer, 0, sizeof(buffer));
+	ret = recv(multi_clients[n], buffer, sizeof(buffer), 0);
 	if(ret < 0)
 	{
 		printf("recv() error\n");
@@ -167,18 +155,16 @@ void server_response(int n, char* ROOT)
 		strcpy(msg, buffer);
 		strcpy(post_msg, buffer);
 
-		// strcpy(POST_msg, buffer);
 		strcpy(always_connect, buffer);
 		reqline[0] = strtok(buffer, " \t\n");
-		printf("reqline0: %s\n", reqline[0]);
+		// printf("reqline0: %s\n", reqline[0]);
 		if(!strncmp(reqline[0], "GET\0", 4))
 		{
 			printf("GET Found\n");
-			// exit(1);
-			reqline[1] = strtok(NULL," \t");
-			printf("reqline1: %s\n", reqline[1]);
-			reqline[2] = strtok(NULL," \t\n");
-			printf("reqline2: %s\n", reqline[2]);
+			reqline[1] = strtok(NULL," \t");			//extract file 
+			// printf("reqline1: %s\n", reqline[1]);
+			reqline[2] = strtok(NULL," \t\n");			//extract HTTP version 
+			// printf("reqline2: %s\n", reqline[2]);
 			if (!strncmp(reqline[2], "HTTP/1.0", 8))
             {
               http = "HTTP/1.0 "; 
@@ -186,7 +172,7 @@ void server_response(int n, char* ROOT)
             if(!strncmp(reqline[2], "HTTP/1.1", 8))
 			{
                http = "HTTP/1.1 ";
-               printf("Selected http is %s\n", http);
+               printf("Client request HTTP version is:%s\n", http);
             }
 
             //400 error: unsupported HTTP version is specified, 
@@ -199,10 +185,6 @@ void server_response(int n, char* ROOT)
   					<p><b>400: Bad Request. Reason - Invalid HTTP-version</b></p>\r\n \
   					</body></html>\r\n");
 				send(multi_clients[n], hdr, strlen(hdr), 0);
-				// strcpy(error_msg,"<HEAD><TITLE>400 Bad Request Reason</TITLE></HEAD>\n<html><BODY>>400 Bad Request Reason: Invalid HTTP-Version:");
-				// strcat(error_msg,reqline[2]);
-				// strcat(error_msg,"\n</BODY></html>");
-				// send(multi_clients[n], error_msg, strlen(error_msg), 0);
             }
             else
             {
@@ -215,24 +197,24 @@ void server_response(int n, char* ROOT)
             		//printf("Copied file is %s\n", tmp_ptr);
             		*(tmp_ptr + 10) = '\0';
             		strcat(def_page,tmp_ptr);
-            		printf("Concat file is %s\n", def_page);
+            		// printf("Concat file is %s\n", def_page);
             		strcpy(reqline[1], def_page);
-            		printf("reqline1 new: %s\n", reqline[1]);
+            		// printf("reqline1 new: %s\n", reqline[1]);
             	}
             	strcpy(file_name, reqline[1]);
-            	printf("File name is %s\n", file_name);
+            	// printf("File name is %s\n", file_name);
             	strncpy(path, ROOT, strlen(ROOT));
             	strcat(path, file_name+1);
-            	printf("Path: %s\n", path);
+            	// printf("Path: %s\n", path);
             	strcpy(file_type, path);
-            	token = strtok(file_type, ".\0 ");
+            	token = strtok(file_type, ".\0 ");	//Finding the requested file type
             	token = strtok(NULL, ".\0");
-            	printf("file type is %s\n", token);
+            	// printf("file type is %s\n", token);
 
             	file_type = strcat(delimiter, token);
-            	printf("Final file format is:%s\n", file_type);
-            	content_type = http_file_format("#Content-Type which the server handles", file_type);
-            	printf("content_type is %s\n", content_type);
+            	// printf("Final file format is:%s\n", file_type);
+            	content_type = http_file_format("#Content-Type which the server handles", file_type);		//Checking whether requested file type is supported by server
+            	// printf("content_type is %s\n", content_type);
 
             	res_fp = fopen(path, "r");
             	if(res_fp != NULL)
@@ -241,29 +223,32 @@ void server_response(int n, char* ROOT)
             		strcpy(hdr, http);
             		strcat(hdr, "200 OK\n");
             		len = file_size(res_fp);
-            		snprintf(size, sizeof(size), "%d", len); //Convert interger to string
-            		printf("Size in string is%s\n", size);
+            		snprintf(size, sizeof(size), "%d", len); 		//Convert integer to string wit(hout itoa()
+            		 // printf("Size in string is%s\n", size);
+            		// sprintf(hdr, "Content-Size :%s\nContent-Type : %s\n\n", size, content_type);
             		strcat(hdr, "Content-Size :");
             		strcat(hdr, size);
             		strcat(hdr, "\n");
             		strcat(hdr, "Content-Type : ");
             		strcat(hdr, content_type);
             		strcat(hdr, "\n\n");
-            		http_server_send(multi_clients[n], hdr, res_fp);
+            		printf("hdr is %s\n", hdr);
+            		http_server_send(multi_clients[n], hdr, res_fp);			//Sending header + data to client
             		fclose(res_fp);
             	}
             	
+            	//error 404: File Not Found
             	else
 				{
 					printf("File NOT FOUND! :(\n");
 					strcpy(hdr, http);
-					strcat(hdr, "404 Not Found\n");
-					strcat(hdr, "Content-Size : NONE\nContent-Type : Invalid\n\n");
+					strcat(hdr, "404 Not Found\nContent-Size : NONE\nContent-Type : Invalid\n\n");
+					// strcat(hdr, "Content-Size : NONE\nContent-Type : Invalid\n\n");
 					send(multi_clients[n], hdr, strlen(hdr), 0);
 					strcpy(error_msg,"<HEAD><TITLE>404 File not found Reason</TITLE></HEAD>\n<html><BODY>404 File not found Request URL doesn't exist:");
 					strcat(error_msg,path);
-					strcat(error_msg,"\n");
-					strcat(error_msg,"</BODY></html>");
+					// strcat(error_msg,"\n");
+					strcat(error_msg,"\n</BODY></html>");
 					send(multi_clients[n], error_msg, strlen(error_msg), 0);
             	}
             }
@@ -272,22 +257,21 @@ void server_response(int n, char* ROOT)
 		else if(!strncmp(reqline[0], "POST\0", 4))
 		{
 			printf("Post request\n");
-			// char *client_msg = strstr(post_msg, "fname=");
 			reqline[1] = strtok(NULL," \t");
 			strcpy(file_name, reqline[1]);
-        	printf("File name is %s\n", file_name);
+        	// printf("File name is %s\n", file_name);
         	strncpy(path, ROOT, strlen(ROOT));
         	strcat(path, file_name+1);
-        	printf("Path: %s\n", path);
+        	// printf("Path: %s\n", path);
         	strcpy(file_type, path);
         	token = strtok(file_type, ".\0 ");
         	token = strtok(NULL, ".\0");
-        	printf("file type is %s\n", token);
+        	// printf("file type is %s\n", token);
 
         	file_type = strcat(delimiter, token);
-        	printf("Final file format is:%s\n", file_type);
+        	// printf("Final file format is:%s\n", file_type);
         	content_type = http_file_format("#Content-Type which the server handles", file_type);
-        	printf("content_type is %s\n", content_type);
+        	// printf("content_type is %s\n", content_type);
 
         	res_fp = fopen(path, "r");
         	if(res_fp != NULL)
@@ -297,20 +281,20 @@ void server_response(int n, char* ROOT)
         		strcat(hdr, "200 OK\n");
         		len = file_size(res_fp);
         		snprintf(size, sizeof(size), "%d", len); //Convert interger to string
-        		printf("Size in string is%s\n", size);
+        		// printf("Size in string is%s\n", size);
         		strcat(hdr, "Content-Size :");
         		strcat(hdr, size);
         		strcat(hdr, "\n");
         		strcat(hdr, "Content-Type : ");
         		strcat(hdr, content_type);
-        		strcat(hdr, "\n\n");
-	    		strcat(hdr, "<html><body><pre><h1>POSTDATA </h1></pre>");
+        		// strcat(hdr, "\n\n");
+	    		strcat(hdr, "\n\n<html><body><pre><h1>POSTDATA </h1></pre>");
         		http_server_send(multi_clients[n], hdr, res_fp);
         		fclose(res_fp);
             }
 
 		}
-		//If neither of the two methods supported is detected, then send error message
+		//501 error: Client requested method is not supported by server
 		 else if (strcmp(reqline[0], "POST") && strcmp(reqline[0],"GET"))
         {
         	printf("Requested method %s not implemented\n", reqline[0]);
@@ -330,19 +314,20 @@ void server_response(int n, char* ROOT)
 			<html><head><title>Aww, snap!</title></head>\r\n<body> \
 			<p><b>500: Internal Server Error - Cannot allocate memory</b></p>\r\n \
 			</body></html>\r\n");
-			//shutdown(sock_connect, 1);
 			send(multi_clients[n], hdr, strlen(hdr), 0);
 			exit(1);
         }
 
         //timeouts
-         if(strstr(always_connect,"keep-alive")!= NULL)
+         if(strstr(always_connect,"Connection: keep-alive")!= NULL)
         {
-        	printf("keep alive is der\n");
-        	time_out = config_file("Keep-Alivetime");
-        	printf("time_out interval is %d\n",atoi(time_out));
-        	printf("timer started\n");
-        	alarm(atoi(time_out));
+    	    // signal(SIGALRM, alarm_handler);
+        	printf("Client requests to keep connection alive\n");
+        	time_out_str = config_file("Keep-Alivetime");	//Extract server timeout from config file
+        	printf("Timeout set is %d\n",atoi(time_out_str));
+        	// printf("timer started\n");
+        	int timeout = atoi(time_out_str);
+        	alarm(timeout);
         }
         else
         {
@@ -353,8 +338,6 @@ void server_response(int n, char* ROOT)
         	exit(0);
         }
 	}
-
-
 }
 
 int main(int argc, char const *argv[])
@@ -377,7 +360,7 @@ int main(int argc, char const *argv[])
 	int server_port = atoi(PORT);
 	printf("Port number is %d\n", server_port);
 
-    signal(SIGALRM, timeout_handler);
+    // signal(SIGALRM, timeout_handler);
 
 	//Create socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -406,11 +389,13 @@ int main(int argc, char const *argv[])
 	printf("Waiting for incoming connection\n");
 	c = sizeof(struct sockaddr_in);
 
-	// Setting all elements to -1: Signifies there are no clients connected
+	// Setting all elements to -1: Signifies that there are no clients connected
 	for (int i = 0; i < NUM_CLIENTS; ++i)
 	{
 		multi_clients[i] = -1;
 	}
+    signal(SIGALRM, alarm_handler);
+
 
 	process_index = 0;
 	
