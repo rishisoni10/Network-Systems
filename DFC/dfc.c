f7475146924cdaa446379fee07f9c349e7fe4bc6
Rishi Soni <rishisoni1003@gmail.com>
2017-11-06 20:15:18 -0700

    Client credentials verifification

--- DFC/dfc.c
+++ DFC/dfc.c
@@ -62,20 +62,24 @@ char* config_file(char *string)
 }
 
 
-int main(){
-  int clientSocket;
-  char buffer[1024];
+int main()
+{
+  int clientSocket, clientSocket_1, clientSocket_2, clientSocket_3, clientSocket_4;
+  char buffer[1024], buffer1[1024], buffer2[1024], buffer3[1024], buffer4[1024];
   struct sockaddr_in serverAddr1, serverAddr2, serverAddr3, serverAddr4;
   socklen_t addr_size;
+  int s1_port, s2_port, s3_port, s4_port;
+  char* key = NULL;
+
 
   /*** Parsing from dfc.conf file***/    
-  char *username = malloc(100);
-  char *password = malloc(100);
-  char *PORT1 = malloc(10);
-  char *PORT2 = malloc(10);
-  char *PORT3 = malloc(10);
-  char *PORT4 = malloc(10);
-  char *temp = malloc(7);
+  char* username = malloc(100);
+  char* password = malloc(100);
+  char* PORT1 = malloc(10);
+  char* PORT2 = malloc(10);
+  char* PORT3 = malloc(10);
+  char* PORT4 = malloc(10);
+  char* temp = malloc(7);
   
   // strcpy(temp, config_file(" "));
   // strcpy(ROOT, config_file("DocumentRoot"));
@@ -85,11 +89,27 @@ int main(){
   strcpy(PORT4, config_file("DFS4"));
   strcpy(username, config_file("Username"));
   strcpy(password, config_file("Password"));
+  strcat(username, ",");
+  strcat(username, password);
+
+  // key = (char*) malloc(strlen(username));
+  // strcpy(key, username);
+  // printf("strcpy is %s", key);
+
 
   printf("Port1:%s Port2:%s Port3:%s Port4:%s\n", PORT1, PORT2, PORT3, PORT4);
 
+  s1_port = atoi(PORT1);
+  s2_port = atoi(PORT2);
+  s3_port = atoi(PORT3);
+  s4_port = atoi(PORT4);
+
   /*---- Create the socket. The three arguments are: ----*/
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  clientSocket_1 = socket(PF_INET, SOCK_STREAM, 0);
+  clientSocket_2 = socket(PF_INET, SOCK_STREAM, 0);
+  clientSocket_3 = socket(PF_INET, SOCK_STREAM, 0);
+  clientSocket_4 = socket(PF_INET, SOCK_STREAM, 0);
+ 
   
   /*---- Configure settings of the server address struct ----*/
   serverAddr1.sin_family = AF_INET;
@@ -98,10 +118,10 @@ int main(){
   serverAddr4.sin_family = AF_INET;
 
   /* Set port number, using htons function to use proper byte order */
-  serverAddr1.sin_port = htons(PORT1);
-  serverAddr2.sin_port = htons(PORT2);
-  serverAddr3.sin_port = htons(PORT3);
-  serverAddr4.sin_port = htons(PORT4);
+  serverAddr1.sin_port = htons(s1_port);
+  serverAddr2.sin_port = htons(s2_port);
+  serverAddr3.sin_port = htons(s3_port);
+  serverAddr4.sin_port = htons(s4_port);
 
   /* Set IP address to localhost for all servers */
   serverAddr1.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -115,16 +135,35 @@ int main(){
   memset(serverAddr3.sin_zero, '\0', sizeof serverAddr3.sin_zero);  
   memset(serverAddr4.sin_zero, '\0', sizeof serverAddr4.sin_zero);  
 
-
-  /*---- Connect the socket to the server using the address struct ----*/
+  /*---- Connect the socket to all servers using the address struct ----*/
   addr_size = sizeof(serverAddr1);
-  connect(clientSocket, (struct sockaddr *) &serverAddr1, addr_size);
+  connect(clientSocket_1, (struct sockaddr *) &serverAddr1, addr_size);
+  connect(clientSocket_2, (struct sockaddr *) &serverAddr2, addr_size);
+  connect(clientSocket_3, (struct sockaddr *) &serverAddr3, addr_size);
+  connect(clientSocket_4, (struct sockaddr *) &serverAddr4, addr_size);
+
 
   /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+  // recv(clientSocket_1, buffer1, 1024, 0);
+  // recv(clientSocket_2, buffer2, 1024, 0);
+  // recv(clientSocket_3, buffer3, 1024, 0);
+  // recv(clientSocket_4, buffer4, 1024, 0);
+  // send(clientSocket_4,username,strlen(username)+1,0);
+  // send(clientSocket_2,username,strlen(username)+1,0);
+  // send(clientSocket_3,username,strlen(username)+1,0);
+  send(clientSocket_1,username,strlen(username)+1,0);
+
+
+
+  // send(clientSocket_1,username,strlen(username),0);
+
 
   /*---- Print the received message ----*/
-  printf("Data received: %s\n",buffer);   
+  // printf("Data received: %s",buffer1);   
+  // printf("Data received: %s",buffer2);   
+  // printf("Data received: %s",buffer3);   
+  // printf("Data received: %s",buffer4);   
+
 
   return 0;
 }
--- dfs.c
+++ dfs.c
@@ -31,20 +31,69 @@
 #include <fcntl.h>
 #include <arpa/inet.h>  //inet_addr
 
-int main(){
+int file_size(FILE *file_fp)
+{
+  fseek(file_fp, 0, SEEK_END);
+  int len = ftell(file_fp);
+  fseek(file_fp, 0, SEEK_SET);
+  return len;
+}
+
+char* config_file(char *string)
+{
+    // printf("Entered config func\n");
+    FILE *conf_fp;
+    char filename[] = "dfs.conf";
+    // char buffer[4000];
+    char *buffer;
+    char *param, *tk;
+    conf_fp = fopen(filename, "r");
+
+    int len = file_size(conf_fp);
+    buffer = malloc(len);
+    fread(buffer, 1, len, conf_fp);
+    if((param = strstr(buffer,string)) != NULL)
+    {
+    tk = strtok(param," \n");
+    // tk = strtok(NULL,":\0");
+    }
+    // printf("Token is:%s\n", tk);
+    fclose(conf_fp);
+    return tk;
+}
+
+int main(int argc, char const *argv[])
+{
   int welcomeSocket, newSocket;
   char buffer[1024];
   struct sockaddr_in serverAddr;
   struct sockaddr_storage serverStorage;
   socklen_t addr_size;
+  int port;
+  char* recv_username = malloc(100);
+  char* recv_password = malloc(100);
+  char* req_username = malloc(100);
+  char* req_password = malloc(100);
+  char* token = malloc(100);
+
 
   /*---- Create the socket. The three arguments are: ----*/
   welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+  if(argc < 2)
+  {
+  	printf("Enter port number of server\n");
+  	exit(1);
+  }
+
+  printf("Value of port is %s\n", argv[1]);
+  port = atoi(argv[1]);
+  printf("Port:%d\n",port);
+
   
   /*---- Configure settings of the server address struct ----*/
   serverAddr.sin_family = AF_INET;
   /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(10001);
+  serverAddr.sin_port = htons(port);
   /* Set IP address to localhost */
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   /* Set all bits of the padding field to 0 */
@@ -54,18 +103,48 @@ int main(){
   bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
   /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
+  while(1)
+  {
+	  if(listen(welcomeSocket,4)==0)
+	    printf("Listening\n");
+	  else
+	    printf("Error\n");
 
-  /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof(serverStorage);
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+	  /*---- Accept call creates a new socket for the incoming connection ----*/
+	  addr_size = sizeof(serverStorage);
+	  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
 
-  /*---- Send message to the socket of the incoming connection ----*/
-  strcpy(buffer,"Hello World\n");
-  send(newSocket,buffer,13,0);
+	  /*---- Receive message from the socket of the incoming connection ----*/
+	  
+	  recv(newSocket,buffer,1024,0);
+
+	  printf("Received info is %s\n", buffer);
+      recv_username = strtok(buffer,",");
+      recv_password = strtok(NULL,",");
+      printf("Received user name:%s\n", recv_username);
+      printf("Received Password:%s\n", recv_password);
+	 
+      req_username = config_file(recv_username);
+      printf("Username token:%s\n", req_username);
+     
+      req_password = config_file(recv_password);
+      printf("Password token:%s\n", req_password);
+
+
+      // printf("\n\n");
+      if(req_password == NULL || req_username == NULL)
+      {
+        printf("Wrong credentials. Try Again\n");
+      }
+      else if(!strcmp(recv_username, req_username) && !strcmp(recv_password, req_password))
+        printf("Client credentials verified\n");
+      else
+        printf("Problem hain\n");
+
+
+
+	  // memset(buffer, 0, sizeof(buffer));
+	}
 
   return 0;
 }
