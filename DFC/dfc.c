8aedbac67b3577925ec416fdfc9a14093ba68104
Rishi Soni <rishisoni1003@gmail.com>
2017-11-08 13:17:39 -0700

    Working MD5HASH for splitting

--- DFC/dfc.c
+++ DFC/dfc.c
@@ -29,141 +29,278 @@
 #include <netdb.h>
 #include <fcntl.h>
 #include <arpa/inet.h>  //inet_addr
+#include <openssl/md5.h>
+
+#define FILENAME_LEN        (50)
+
+char command[100];
+
+unsigned int MD5HASH(FILE *fp)
+{
+    int i, bytes;
+    MD5_CTX mdContext;
+    unsigned long mask;
+    unsigned char c[MD5_DIGEST_LENGTH];
+    unsigned char data[1024];
+    unsigned char* md5_mask = malloc(sizeof(char) * 4);
+
+    MD5_Init(&mdContext);
+    while((bytes = fread(data, 1, 1024, fp)) != 0)
+        MD5_Update(&mdContext, data, bytes) ;
+    MD5_Final(c, &mdContext);
+
+    for (i = 0; i < MD5_DIGEST_LENGTH; i++)
+    {
+        printf("%x",c[i]);;
+    }
+
+    // fclose(fp);
+    printf("\n");
+
+    // printf("Md5_mask:\n");
+    // for(i = 0; i < 4; i++)
+    // {
+    //     md5_mask[i] = c[i];
+    //     printf("%d ", md5_mask[i]);
+    // }
+    // md5_mask[4] = '\0';
+    // printf("\n");
+    // i = 0;
+    // while(md5_mask[i] !='\0')
+    // {
+    //     printf("%x", md5_mask[i]);
+    //     i++;
+    // }
+
+    //Extracting first 4 bytes of md5sum string
+    md5_mask = strncpy(md5_mask, c+12, 4);
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d ", md5_mask[i]);
+    }
+    mask = 0;
+    for(i = 0; i < 4; i++)
+    {
+        if(md5_mask[i] < 100)
+            mask = (mask * 100) + md5_mask[i];
+        else //if (md5_mask[i] >=100)
+            mask = (mask * 1000) + md5_mask[i];
+        
+        printf("Result of multiplication: %ld\n", mask);
+    }
+    
+    printf("\nMask is %ld\n", mask);
+    return mask;
+}
+
 
 
 int file_size(FILE *file_fp)
 {
-  fseek(file_fp, 0, SEEK_END);
-  int len = ftell(file_fp);
-  fseek(file_fp, 0, SEEK_SET);
-  return len;
+    fseek(file_fp, 0, SEEK_END);
+    int len = ftell(file_fp);
+    fseek(file_fp, 0, SEEK_SET);
+    return len;
 }
 
 char* config_file(char *string)
 {
-  FILE *conf_fp;
-  char filename[] = "dfc.conf";
-  // char buffer[4000];
-  char *buffer;
-  char *param, *tk;
-  conf_fp = fopen(filename, "r");
+    FILE *conf_fp;
+    char filename[] = "dfc.conf";
+    // char buffer[4000];
+    char *buffer;
+    char *param, *tk;
+    conf_fp = fopen(filename, "r");
 
-  int len = file_size(conf_fp);
-  buffer = malloc(len);
-  fread(buffer, 1, len, conf_fp);
-  if((param = strstr(buffer,string)) != NULL)
-  {
+    int len = file_size(conf_fp);
+    buffer = malloc(len);
+    fread(buffer, 1, len, conf_fp);
+    if((param = strstr(buffer,string)) != NULL)
+    {
     tk = strtok(param,":\n");
     tk = strtok(NULL,":\n");
-  }
-  // printf("Token is:%s\n", tk);
-  fclose(conf_fp);
-  return tk;
+    }
+    // printf("Token is:%s\n", tk);
+    fclose(conf_fp);
+    return tk;
 }
 
 
-int main()
+int main(int argc, char const *argv[])
 {
-  int clientSocket, clientSocket_1, clientSocket_2, clientSocket_3, clientSocket_4;
-  char buffer[1024], buffer1[1024], buffer2[1024], buffer3[1024], buffer4[1024];
-  struct sockaddr_in serverAddr1, serverAddr2, serverAddr3, serverAddr4;
-  socklen_t addr_size;
-  int s1_port, s2_port, s3_port, s4_port;
-  char* key = NULL;
+    int clientSocket, clientSocket_1, clientSocket_2, clientSocket_3, clientSocket_4;
+    char buffer[1024], buffer1[1024], buffer2[1024], buffer3[1024], buffer4[1024];
+    struct sockaddr_in serverAddr1, serverAddr2, serverAddr3, serverAddr4;
+    socklen_t addr_size;
+    int s1_port, s2_port, s3_port, s4_port;
+    char* key = NULL;
+    unsigned int x;
+    char reply;
+    char filename[FILENAME_LEN];
+    int len;
+   
+    FILE* fp = NULL;
+
+     if(argc < 2)
+    {
+        printf("Enter file to be sent.\n");
+        exit(1);
+    }
+    strcpy(filename, argv[1]);
+
+    /*** Parsing from dfc.conf file***/    
+    char* username = malloc(100);
+    char* password = malloc(100);
+    char* PORT1 = malloc(10);
+    char* PORT2 = malloc(10);
+    char* PORT3 = malloc(10);
+    char* PORT4 = malloc(10);
+    char* temp = malloc(7);
+
+    // strcpy(temp, config_file(" "));
+    // strcpy(ROOT, config_file("DocumentRoot"));
+    strcpy(PORT1, config_file("DFS1"));
+    strcpy(PORT2, config_file("DFS2"));
+    strcpy(PORT3, config_file("DFS3"));
+    strcpy(PORT4, config_file("DFS4"));
+    strcpy(username, config_file("Username"));
+    strcpy(password, config_file("Password"));
+    strcat(username, ",");
+    strcat(username, password);
+
+    // key = (char*) malloc(strlen(username));
+    // strcpy(key, username);
+    // printf("strcpy is %s", key);
 
 
-  /*** Parsing from dfc.conf file***/    
-  char* username = malloc(100);
-  char* password = malloc(100);
-  char* PORT1 = malloc(10);
-  char* PORT2 = malloc(10);
-  char* PORT3 = malloc(10);
-  char* PORT4 = malloc(10);
-  char* temp = malloc(7);
-  
-  // strcpy(temp, config_file(" "));
-  // strcpy(ROOT, config_file("DocumentRoot"));
-  strcpy(PORT1, config_file("DFS1"));
-  strcpy(PORT2, config_file("DFS2"));
-  strcpy(PORT3, config_file("DFS3"));
-  strcpy(PORT4, config_file("DFS4"));
-  strcpy(username, config_file("Username"));
-  strcpy(password, config_file("Password"));
-  strcat(username, ",");
-  strcat(username, password);
+    printf("Port1:%s Port2:%s Port3:%s Port4:%s\n", PORT1, PORT2, PORT3, PORT4);
 
-  // key = (char*) malloc(strlen(username));
-  // strcpy(key, username);
-  // printf("strcpy is %s", key);
+    s1_port = atoi(PORT1);
+    s2_port = atoi(PORT2);
+    s3_port = atoi(PORT3);
+    s4_port = atoi(PORT4);
+
+    /*---- Create the socket. The three arguments are: ----*/
+    clientSocket_1 = socket(PF_INET, SOCK_STREAM, 0);
+    clientSocket_2 = socket(PF_INET, SOCK_STREAM, 0);
+    clientSocket_3 = socket(PF_INET, SOCK_STREAM, 0);
+    clientSocket_4 = socket(PF_INET, SOCK_STREAM, 0);
 
 
-  printf("Port1:%s Port2:%s Port3:%s Port4:%s\n", PORT1, PORT2, PORT3, PORT4);
+    /*---- Configure settings of the server address struct ----*/
+    serverAddr1.sin_family = AF_INET;
+    serverAddr2.sin_family = AF_INET;
+    serverAddr3.sin_family = AF_INET;
+    serverAddr4.sin_family = AF_INET;
 
-  s1_port = atoi(PORT1);
-  s2_port = atoi(PORT2);
-  s3_port = atoi(PORT3);
-  s4_port = atoi(PORT4);
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr1.sin_port = htons(s1_port);
+    serverAddr2.sin_port = htons(s2_port);
+    serverAddr3.sin_port = htons(s3_port);
+    serverAddr4.sin_port = htons(s4_port);
 
-  /*---- Create the socket. The three arguments are: ----*/
-  clientSocket_1 = socket(PF_INET, SOCK_STREAM, 0);
-  clientSocket_2 = socket(PF_INET, SOCK_STREAM, 0);
-  clientSocket_3 = socket(PF_INET, SOCK_STREAM, 0);
-  clientSocket_4 = socket(PF_INET, SOCK_STREAM, 0);
- 
-  
-  /*---- Configure settings of the server address struct ----*/
-  serverAddr1.sin_family = AF_INET;
-  serverAddr2.sin_family = AF_INET;
-  serverAddr3.sin_family = AF_INET;
-  serverAddr4.sin_family = AF_INET;
+    /* Set IP address to localhost for all servers */
+    serverAddr1.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr2.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr3.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr4.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr1.sin_port = htons(s1_port);
-  serverAddr2.sin_port = htons(s2_port);
-  serverAddr3.sin_port = htons(s3_port);
-  serverAddr4.sin_port = htons(s4_port);
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr1.sin_zero, '\0', sizeof serverAddr1.sin_zero);  
+    memset(serverAddr2.sin_zero, '\0', sizeof serverAddr2.sin_zero);  
+    memset(serverAddr3.sin_zero, '\0', sizeof serverAddr3.sin_zero);  
+    memset(serverAddr4.sin_zero, '\0', sizeof serverAddr4.sin_zero);  
 
-  /* Set IP address to localhost for all servers */
-  serverAddr1.sin_addr.s_addr = inet_addr("127.0.0.1");
-  serverAddr2.sin_addr.s_addr = inet_addr("127.0.0.1");
-  serverAddr3.sin_addr.s_addr = inet_addr("127.0.0.1");
-  serverAddr4.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr1.sin_zero, '\0', sizeof serverAddr1.sin_zero);  
-  memset(serverAddr2.sin_zero, '\0', sizeof serverAddr2.sin_zero);  
-  memset(serverAddr3.sin_zero, '\0', sizeof serverAddr3.sin_zero);  
-  memset(serverAddr4.sin_zero, '\0', sizeof serverAddr4.sin_zero);  
-
-  /*---- Connect the socket to all servers using the address struct ----*/
-  addr_size = sizeof(serverAddr1);
-  connect(clientSocket_1, (struct sockaddr *) &serverAddr1, addr_size);
-  connect(clientSocket_2, (struct sockaddr *) &serverAddr2, addr_size);
-  connect(clientSocket_3, (struct sockaddr *) &serverAddr3, addr_size);
-  connect(clientSocket_4, (struct sockaddr *) &serverAddr4, addr_size);
+    /*---- Connect the socket to all servers using the address struct ----*/
+    addr_size = sizeof(serverAddr1);
+    connect(clientSocket_1, (struct sockaddr *) &serverAddr1, addr_size);
+    connect(clientSocket_2, (struct sockaddr *) &serverAddr2, addr_size);
+    connect(clientSocket_3, (struct sockaddr *) &serverAddr3, addr_size);
+    connect(clientSocket_4, (struct sockaddr *) &serverAddr4, addr_size);
 
 
-  /*---- Read the message from the server into the buffer ----*/
-  // recv(clientSocket_1, buffer1, 1024, 0);
-  // recv(clientSocket_2, buffer2, 1024, 0);
-  // recv(clientSocket_3, buffer3, 1024, 0);
-  // recv(clientSocket_4, buffer4, 1024, 0);
-  // send(clientSocket_4,username,strlen(username)+1,0);
-  // send(clientSocket_2,username,strlen(username)+1,0);
-  // send(clientSocket_3,username,strlen(username)+1,0);
-  send(clientSocket_1,username,strlen(username)+1,0);
+    /*---- Send user credentials from client to all servers ----*/
+    send(clientSocket_1,username,strlen(username)+1,0);
+
+    recv(clientSocket_1, &reply, 1, 0);
+    
+    if(reply == '0')
+    {
+        printf("User credentials wrong\n");
+        exit(1);
+    }
+    else if(reply == '1')
+        printf("User credentials correct\n");
+
+
+    fp = fopen(filename, "r");
+    if(fp == NULL)
+    {
+        printf("File cannot opened / file does not exist\n");
+        return 1;
+    }   
+
+    // printf("Return value is %d\n", MD5HASH(fp));
+    x = MD5HASH(fp) % 4;
+    printf("x is %d\n",x);
+    fclose(fp);
+
+    while(1)
+    {
+        memset(command, 0, sizeof(command));
+        printf("\r\n     *** Main Menu ***\n\r");
+        printf("Enter the following commands for file transfers / handling\n");
+        printf("get <file_name>\n");
+        printf("put <file_name>\n");
+        printf("list\n");
+        printf("Type in the command followed by the <file_name>, if the command requires\n");
+
+        char *file_name = malloc(10);
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = 0;
+        printf("Sending entered command to server\n");
+        send(clientSocket_1,command,strlen(command)+1,0);
+        memset(command, 0, sizeof(command));
+        recv(clientSocket_1, command, 1024, 0);
 
 
 
-  // send(clientSocket_1,username,strlen(username),0);
+        if(x == 0)
+        {
+            printf("0 case\n");
+            fp = fopen(filename, "r");
+            len = file_size(fp);
+            char* P1 = (char*)malloc(len/4);
+            char* P2 = (char*)malloc(len/4);
+            char* P3 = (char*)malloc(len/4);
+            char* P4 = (char*)malloc(len/4);
+            fread(P1, 1, (len/4), fp);
+            fread(P2, 1, (len/4), fp);
+            fread(P3, 1, (len/4), fp);
+            fread(P4, 1, (len/4), fp);
+        }
+
+        if (x == 1)
+        {
+            printf("1 case\n");
+        }
+        if (x == 2)
+        {
+            printf("2 case\n");
+        }
+        if(x == 3)
+        {
+            printf("3 case\n");
+        }
+    }
+    
+
+    /*---- Print the received message ----*/
+    // printf("Data received: %s",buffer1);   
+    // printf("Data received: %s",buffer2);   
+    // printf("Data received: %s",buffer3);   
+    // printf("Data received: %s",buffer4);   
 
 
-  /*---- Print the received message ----*/
-  // printf("Data received: %s",buffer1);   
-  // printf("Data received: %s",buffer2);   
-  // printf("Data received: %s",buffer3);   
-  // printf("Data received: %s",buffer4);   
-
-
-  return 0;
+    return 0;
 }
--- dfs.c
+++ dfs.c
@@ -31,12 +31,15 @@
 #include <fcntl.h>
 #include <arpa/inet.h>  //inet_addr
 
+int nbytes;                        //number of bytes we receive in our message
+char file_name[10];
+
 int file_size(FILE *file_fp)
 {
-  fseek(file_fp, 0, SEEK_END);
-  int len = ftell(file_fp);
-  fseek(file_fp, 0, SEEK_SET);
-  return len;
+    fseek(file_fp, 0, SEEK_END);
+    int len = ftell(file_fp);
+    fseek(file_fp, 0, SEEK_SET);
+    return len;
 }
 
 char* config_file(char *string)
@@ -62,6 +65,114 @@ char* config_file(char *string)
     return tk;
 }
 
+/**
+* @brief Receives file from server
+*
+* Given a file name, this function receives the file from the client
+*
+* @param *file_name     File name pointer
+*        sockfd         Socket ID
+*        remote         Socket parameters
+*
+* @return void 
+*/
+/*
+void put_file(char *file_name, int sockfd)
+{
+    char msg[100];
+    int index_req, old_index;
+    remote_length = sizeof(remote);
+    memset(msg,0, sizeof(msg));
+    nbytes = recvfrom(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&remote, &remote_length);
+    printf("put function at server receives: %s\n", msg);
+
+    if(!strcmp(msg, "Sending file"))
+    {
+        printf("Client reply received\n");
+        memset(msg,0, sizeof(msg));
+        strcpy(msg, "Okay");
+        printf("Sending ACK: %s\n", msg);
+        nbytes = sendto(sockfd, msg, (sizeof(msg) - 1), 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+    }
+
+    memset(&file_size, 0, sizeof(file_size));
+    nbytes = recvfrom(sockfd, &file_size, sizeof(file_size), 0, (struct sockaddr *)&remote, &remote_length);
+    printf("The size of the file to be received is %ld\n", file_size);
+    
+    pkt_size = sizeof(pkt->buffer) + sizeof(pkt->index) + sizeof(pkt->data_length);
+    pkt = (PACKET *)malloc(pkt_size);
+
+    printf("Writing received data in %s\n", file_name);
+    fp = fopen(file_name+1, "w");
+
+    index_req = 1;
+    int loop_count = 0;
+
+
+    //Setting the timeout using setsockopt()
+    // timeout.tv_sec = 0;
+    // timeout.tv_usec = 500000;  //500ms timeout
+    // setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+
+    //Loop till entire file has been received
+    while(file_size > 0)
+    {
+        loop_count = 0;
+        nbytes = recvfrom(sockfd, pkt, pkt_size, 0, (struct sockaddr *)&remote, &remote_length);
+        if(pkt->index == index_req)
+        {
+            printf("Correct data received\n");
+            printf("Received index is %d\n", pkt->index);
+
+            //64-bit decryption. double XOR every byte in packet to recover original data
+            while(loop_count<pkt_size)
+            {
+                pkt->buffer[loop_count] ^= key[loop_count % (key_len-1)] ^ key[loop_count % (key_len-1)];
+                ++loop_count;
+            }
+
+            fwrite(pkt->buffer, 1, pkt->data_length, fp);
+            file_size = file_size - pkt->data_length;
+            printf("Current file size is %ld\n", file_size);
+            memset(pkt, 0, pkt_size);
+            pkt->index = index_req;
+            nbytes = sendto(sockfd, pkt, pkt_size, 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+            index_req++;
+        }
+
+        //If old packet has been received, send ACK of old packet
+        else if (pkt->index < index_req)
+        {
+            printf("Old pkt received\n");
+            printf("Received index: %d\n", pkt->index);
+            old_index = pkt->index;
+            memset(pkt, 0, pkt_size);
+            pkt->index = old_index;
+            nbytes = sendto(sockfd, pkt, pkt_size, 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+        }
+        
+        //Else, index=0. This makes sender loop till it receives correct ACK
+        else
+        {
+            printf("Incorrect data received. Send data again\n");
+            // strcpy(pkt->buffer, "Incorrect index. Send again");
+            printf("Received index: %d\n", pkt->index);
+            memset(pkt, 0, pkt_size);
+            pkt->index = 0;
+            nbytes = sendto(sockfd, pkt, pkt_size, 0, (struct sockaddr *)&remote, sizeof(struct sockaddr));
+            // printf("Incorrect Received index is %d\n", pkt->index);
+        }
+        memset(pkt, 0, pkt_size);
+    }
+    // timeout.tv_sec = 0;
+    // timeout.tv_usec = 0;
+    // setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+    fclose(fp);
+    printf("Client to Server file transfer complete\n");
+}
+*/
+
+
 int main(int argc, char const *argv[])
 {
     int welcomeSocket, newSocket;
@@ -70,6 +181,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_storage serverStorage;
     socklen_t addr_size;
     int port;
+    char ACK;
     char* recv_username = malloc(100);
     char* recv_password = malloc(100);
     char* req_username = malloc(100);
@@ -121,6 +233,8 @@ int main(int argc, char const *argv[])
 
         recv(newSocket,buffer,1024,0);
 
+
+        /* ------User credentials check ----------*/
         printf("Received info is %s\n", buffer);
         recv_username = strtok(buffer,",");
         recv_password = strtok(NULL,",");
@@ -135,40 +249,61 @@ int main(int argc, char const *argv[])
         if((cpy_1 = strstr(file_contents, recv_username)) != NULL)
         {
             req_username = strtok(cpy_1, " ,\n");
-            // req_username = strtok(NULL, ",");
-
             if(strcmp(req_username, recv_username) == 0)
-                printf("Username checks out:%s\n", req_username);
+                printf("Username checks out\n");
             else
-                printf("Error in username:%s\n", req_username);
+                printf("Error in username\n");
         }
         else
-            printf("Username not found:%s\n", req_username);
+            printf("Username not found\n");
 
         if((cpy_2 = strstr(file_contents, recv_password)) != NULL)
         {
             req_password = strtok(cpy_2, ",");
             // req_password = strtok(NULL, ",\n");
             if(strcmp(req_password, recv_password) == 0)
-                printf("Password checks out:%s\n", req_password);
+                printf("Password checks out\n");
             else
-                printf("Error in Password:%s\n", req_password);
+                printf("Error in Password:\n");
         }
         else
-            printf("Password not found:%s\n", req_password);
-
+            printf("Password not found\n");
 
         if(req_password == NULL || req_username == NULL)
         {
             printf("Wrong credentials. Try Again\n");   // username/password not found in dfs.conf
         }
         else if(!strcmp(recv_username, req_username) && !strcmp(recv_password, req_password))
+        {
             printf("Client credentials verified\n");
+            ACK = '1';
+            send(newSocket,&ACK,1,0);
+            break; 
+        }
         else
+        {
             printf("Wrong credentials. Try Again....\n");
-
+            ACK = '0';
+            send(newSocket,&ACK,1,0);
+        }
         // memset(buffer, 0, sizeof(buffer));
     }
+
+    memset(buffer, 0, sizeof(buffer));
+    recv(newSocket,buffer,1024,0);
+    if(strstr(buffer, "put") != NULL)
+    {
+        printf("Found command\n");
+        strcpy(file_name, (buffer + 3));
+        memset(buffer,0, sizeof(buffer));
+        strcpy(buffer, "Send file");
+        strcat(buffer, file_name);
+        printf("Sending the following string: %s\n", buffer);
+        send(newSocket,buffer,1024,0);
+        // put_file(file_name, sockfd, remote);
+    }
+
+
 
     return 0;
 }
